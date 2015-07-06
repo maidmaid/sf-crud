75c2a4d6f6d94f9bc99cdfe36681c25f84bf7022
Dany Maillard <danymaillard93b@gmail.com>
2015-07-06 23:02:01 +0200

    Initial

---
+++ README.md
@@ -0,0 +1,4 @@
+sf-crud
+=======
+
+A Symfony project created on July 6, 2015, 10:23 pm.
---
+++ src/AppBundle/Controller/ArticleController.php
@@ -0,0 +1,239 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use AppBundle\Entity\Article;
+use AppBundle\Form\ArticleType;
+
+/**
+ * Article controller.
+ *
+ * @Route("/article")
+ */
+class ArticleController extends Controller
+{
+    /**
+     * Lists all Article entities.
+     *
+     * @Route("/", name="article")
+     * @Method("GET")
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $articles = $em->getRepository('AppBundle:Article')->findAll();
+
+        return $this->render('AppBundle:Article:index.html.twig', array(
+            'articles' => $articles,
+        ));
+    }
+    /**
+     * Creates a new Article entity.
+     *
+     * @Route("/", name="article_create")
+     * @Method("POST")
+     */
+    public function createAction(Request $request)
+    {
+        $article = new Article();
+        $form = $this->createCreateForm($article);
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($article);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('article_show', array('id' => $article->getId())));
+        }
+
+        return $this->render('AppBundle:Article:new.html.twig', array(
+            'article' => $article,
+            'form'   => $form->createView(),
+        ));
+    }
+
+    /**
+     * Creates a form to create a Article entity.
+     *
+     * @param Article $article The entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createCreateForm(Article $article)
+    {
+        $form = $this->createForm(new ArticleType(), $article, array(
+            'action' => $this->generateUrl('article_create'),
+            'method' => 'POST',
+        ));
+
+        $form->add('submit', 'submit', array('label' => 'Create'));
+
+        return $form;
+    }
+
+    /**
+     * Displays a form to create a new Article entity.
+     *
+     * @Route("/new", name="article_new")
+     * @Method("GET")
+     */
+    public function newAction()
+    {
+        $article = new Article();
+        $form   = $this->createCreateForm($article);
+
+        return $this->render('AppBundle:Article:new.html.twig', array(
+            'article' => $article,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a Article entity.
+     *
+     * @Route("/{id}", name="article_show")
+     * @Method("GET")
+     */
+    public function showAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $article = $em->getRepository('AppBundle:Article')->find($id);
+
+        if (!$article) {
+            throw $this->createNotFoundException('Unable to find Article entity.');
+        }
+
+        $deleteForm = $this->createDeleteForm($id);
+
+        return $this->render('AppBundle:Article:show.html.twig', array(
+            'article' => $article,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing Article entity.
+     *
+     * @Route("/{id}/edit", name="article_edit")
+     * @Method("GET")
+     */
+    public function editAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $article = $em->getRepository('AppBundle:Article')->find($id);
+
+        if (!$article) {
+            throw $this->createNotFoundException('Unable to find Article entity.');
+        }
+
+        $editForm = $this->createEditForm($article);
+        $deleteForm = $this->createDeleteForm($id);
+
+        return $this->render('AppBundle:Article:edit.html.twig', array(
+            'article' => $article,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+    * Creates a form to edit a Article entity.
+    *
+    * @param Article $article The entity
+    *
+    * @return \Symfony\Component\Form\Form The form
+    */
+    private function createEditForm(Article $article)
+    {
+        $form = $this->createForm(new ArticleType(), $article, array(
+            'action' => $this->generateUrl('article_update', array('id' => $article->getId())),
+            'method' => 'PUT',
+        ));
+
+        $form->add('submit', 'submit', array('label' => 'Update'));
+
+        return $form;
+    }
+    /**
+     * Edits an existing Article entity.
+     *
+     * @Route("/{id}", name="article_update")
+     * @Method("PUT")
+     */
+    public function updateAction(Request $request, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $article = $em->getRepository('AppBundle:Article')->find($id);
+
+        if (!$article) {
+            throw $this->createNotFoundException('Unable to find Article entity.');
+        }
+
+        $deleteForm = $this->createDeleteForm($id);
+        $editForm = $this->createEditForm($article);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isValid()) {
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('article_edit', array('id' => $id)));
+        }
+
+        return $this->render('AppBundle:Article:edit.html.twig', array(
+            'article' => $article,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+    /**
+     * Deletes a Article entity.
+     *
+     * @Route("/{id}", name="article_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, $id)
+    {
+        $form = $this->createDeleteForm($id);
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $article = $em->getRepository('AppBundle:Article')->find($id);
+
+            if (!$article) {
+                throw $this->createNotFoundException('Unable to find Article entity.');
+            }
+
+            $em->remove($article);
+            $em->flush();
+        }
+
+        return $this->redirect($this->generateUrl('article'));
+    }
+
+    /**
+     * Creates a form to delete a Article entity by id.
+     *
+     * @param mixed $id The entity id
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm($id)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('article_delete', array('id' => $id)))
+            ->setMethod('DELETE')
+            ->add('submit', 'submit', array('label' => 'Delete'))
+            ->getForm()
+        ;
+    }
+}
