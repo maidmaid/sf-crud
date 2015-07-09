8110f3ac93bfb4a3a75e3e910808e876ac12a968
Dany Maillard <danymaillard93b@gmail.com>
2015-07-09 10:08:54 +0200

    Update routing in php format

--- src/AppBundle/Resources/config/routing/article.php
+++ src/AppBundle/Resources/config/routing/article.php
@@ -5,50 +5,54 @@ use Symfony\Component\Routing\Route;
 
 $collection = new RouteCollection();
 
-$collection->add('article', new Route('/', array(
-    '_controller' => 'AppBundle:Article:index',
-)));
-
-$collection->add('article_show', new Route('/{id}/show', array(
-    '_controller' => 'AppBundle:Article:show',
-)));
-
-$collection->add('article_new', new Route('/new', array(
-    '_controller' => 'AppBundle:Article:new',
-)));
-
-$collection->add('article_create', new Route(
-    '/create',
-    array('_controller' => 'AppBundle:Article:create'),
+$collection->add('article_index', new Route(
+    '/',
+    array('_controller' => 'AppBundle:Article:index'),
     array(),
     array(),
     '',
     array(),
-    'POST'
+    array('GET')
 ));
 
-$collection->add('article_edit', new Route('/{id}/edit', array(
-    '_controller' => 'AppBundle:Article:edit',
-)));
+$collection->add('article_show', new Route(
+    '/{id}/show',
+    array('_controller' => 'AppBundle:Article:show'),
+    array(),
+    array('id' => '\d+'),
+    '',
+    array(),
+    array('GET')
+));
 
-$collection->add('article_update', new Route(
-    '/{id}/update',
-    array('_controller' => 'AppBundle:Article:update'),
+$collection->add('article_new', new Route(
+    '/new',
+    array('_controller' => 'AppBundle:Article:new'),
     array(),
     array(),
     '',
     array(),
-    array('POST', 'PUT')
+    array('GET', 'POST')
+));
+
+$collection->add('article_edit', new Route(
+    '/{id}/edit',
+    array('_controller' => 'AppBundle:Article:edit'),
+    array(),
+    array('id' => '\d+'),
+    '',
+    array(),
+    array('GET', 'POST')
 ));
 
 $collection->add('article_delete', new Route(
     '/{id}/delete',
     array('_controller' => 'AppBundle:Article:delete'),
     array(),
-    array(),
+    array('id' => '\d+'),
     '',
     array(),
-    array('POST', 'DELETE')
+    array('DELETE')
 ));
 
 return $collection;
